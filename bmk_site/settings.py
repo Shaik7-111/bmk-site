2397b350490cc8c5794dd744d590ab4642913893
Shaik Ismail Hussain <shaikismailhussain669@gmail.com>
2026-01-21 21:03:11 +0530

    logo fixed

--- bmk_site/settings.py
+++ bmk_site/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+import dj_database_url
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -105,12 +106,23 @@ WSGI_APPLICATION = 'bmk_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=False,
+    )
 }
+
+
+
 
 # DATABASES = {
 #     'default': {
