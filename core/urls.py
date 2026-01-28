85b7361d95261d80945f06b5b0f2de37d939c79f
Shaik Ismail Hussain <shaikismailhussain669@gmail.com>
2026-01-28 16:06:23 +0530

    logo fixed

--- core/urls.py
+++ core/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
     path('wishlist/add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
     path('wishlist/remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
-    path("create-admin/", views.create_admin_once)
 
 
 ]
--- static/js/products.js
+++ static/js/products.js
@@ -2,8 +2,20 @@ function toggleCategories() {
   const panel = document.getElementById("categoriesPanel");
   const arrow = document.getElementById("catArrow");
 
-  if (!panel) return;
+  if (!panel || !arrow) return;
 
   panel.classList.toggle("hide");
   arrow.classList.toggle("rotate");
 }
+
+//
+//
+//function openFilter() {
+//  document.getElementById("filterDrawer").classList.add("open");
+//  document.getElementById("filterOverlay").classList.add("show");
+//}
+//
+//function closeFilter() {
+//  document.getElementById("filterDrawer").classList.remove("open");
+//  document.getElementById("filterOverlay").classList.remove("show");
+//}
